a0dac921cd3c177c753ea6171d9fc772af639dc6
aniltcs391243 <anil.kothari@tcs.com>
2015-07-17 09:37:06 +0530

    AppDelegate
    AppDelegate

--- AKWeather/AppDelegate.swift
+++ AKWeather/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  AKWeather
 //
 //  Created by Anil Kothari on 14/07/15.
-//  Copyright (c) 2015 Rakesh Kothari. All rights reserved.
+//  Copyright (c) 2015 Anil Kothari. All rights reserved.
 //
 
 import UIKit
